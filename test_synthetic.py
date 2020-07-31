bd731379b6f8c77c28cc8601cc09de1ab337ca00
Mathieu Leocmach <mathieu.leocmach@univ-lyon1.fr>
2020-07-31 14:36:34 +0200

    Test A1, b1 and c1 on symmetries rather than values

--- test_synthetic.py
+++ test_synthetic.py
@@ -2,13 +2,41 @@ import numpy as np
 import pytest
 from make_Abc_fast import make_Abc_fast
 from estimate_displacement import estimate_displacement
+from prepare_displacement_matrices import prepare_displacement_matrices
 
 def test_slingle_square():
     """move a single square by 1 pixel"""
     im0 = np.zeros((64,64))
     im0[30:33, 32:35] = 1
     A1, b1, c1 = make_Abc_fast(im0, 5)
-    assert np.abs(c1[31,32]-1) <0.1
+    #c1
+    assert np.unravel_index(np.argmax(c1), im0.shape) == (31,33)
+    #b1
+    assert np.unravel_index(np.argmin(b1[...,0]), im0.shape) == (31,31)
+    assert np.unravel_index(np.argmax(b1[...,0]), im0.shape) == (31,34)
+    assert np.unravel_index(np.argmin(b1[...,1]), im0.shape) == (29,33)
+    assert np.unravel_index(np.argmax(b1[...,1]), im0.shape) == (32,33)
+    #A1 diagonal
+    assert np.unravel_index(np.argmax(A1[...,0,0]), im0.shape) in [(31,31), (31,35)]
+    assert A1[31,31,0,0] == A1[31,35,0,0]
+    assert np.unravel_index(np.argmin(A1[...,0,0]), im0.shape) in [(31,32), (31,34)]
+    assert A1[31,32,0,0] == A1[31,34,0,0]
+    assert np.unravel_index(np.argmax(A1[...,1,1]), im0.shape) in [(29,33), (33,33)]
+    assert A1[29,33,1,1] == A1[33,33,1,1]
+    assert np.unravel_index(np.argmin(A1[...,1,1]), im0.shape) in [(30,33), (32,33)]
+    assert A1[30,33,1,1] == A1[32,33,1,1]
+    assert A1[31,31,0,0] == A1[33,33,1,1]
+    #A1 nondiagonal
+    assert np.all(A1[...,0,1] == A1[...,1,0])
+    A101M = [(29,31), (29,32), (30,31), (30,32), (32,34), (32,35), (33, 34), (33,35)]
+    assert np.unravel_index(np.argmax(A1[...,0,1]), im0.shape) in A101M
+    for i,j in A101M[1:]:
+        assert A1[i,j,0,1] == A1[29,31,0,1]
+    A101m = [(29,34), (29,35), (30,34), (30,35), (32,31), (32,32), (33, 31), (33,32)]
+    assert np.unravel_index(np.argmin(A1[...,0,1]), im0.shape) in A101m
+    for i,j in A101m[1:]:
+        assert A1[i,j,0,1] == A1[29,34,0,1]
+
 
     #identical images should not detect displacement
     im1 = np.copy(im0)
