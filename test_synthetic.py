3e0341e4b090ab2ac9ceb1b484ad75b9d7685380
Mathieu Leocmach <mathieu.leocmach@gmail.com>
2020-11-12 13:42:21 +0100

    Tests for 3D

--- test_synthetic.py
+++ test_synthetic.py
@@ -75,15 +75,15 @@ def test_single_square_fast():
     assert np.all(Delta_b == -0.5*(b1-b0))
     assert np.all(A == 0.5*(A0+A1))
     displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="fast")
-    assert int(displ[31,33,0]) == 1
-    assert int(displ[31,33,1]) == 0
-
-    #d0 = np.zeros_like(displ)
-    #d0[...,1] = 1
-    #d0 = displ
-    #displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="fast", d0=d0)
-    #assert int(displ[31,33,0]) == 0
-    #assert int(displ[31,33,1]) == 1
+    np.testing.assert_almost_equal(displ[31,33,0], 1,1)
+    np.testing.assert_almost_equal(displ[31,33,1], 0)
+    #initial guess of the displacement
+    d0 = np.zeros_like(displ)
+    d0[...,0] = 1
+    d0 = displ
+    displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="fast", d0=d0)
+    np.testing.assert_almost_equal(displ[31,33,0], 1,0)
+    np.testing.assert_almost_equal(displ[31,33,1], 0)
 
     #shift by one pixel on axis 1
     im1 = np.zeros((64,64))
@@ -97,8 +97,15 @@ def test_single_square_fast():
     assert np.all(Delta_b == -0.5*(b1-b0))
     assert np.all(A == 0.5*(A0+A1))
     displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="accurate")
-    assert int(displ[31,33,0]) == 0
-    assert int(displ[31,33,1]) == 1
+    np.testing.assert_almost_equal(displ[31,33,0], 0)
+    np.testing.assert_almost_equal(displ[31,33,1], 1,1)
+    #initial guess of the displacement
+    d0 = np.zeros_like(displ)
+    d0[...,1] = 1
+    d0 = displ
+    displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="fast", d0=d0)
+    np.testing.assert_almost_equal(displ[31,33,0], 0)
+    np.testing.assert_almost_equal(displ[31,33,1], 1,0)
 
 def test_single_square_accurate():
     """move a single square by 1 pixel"""
@@ -121,3 +128,77 @@ def test_single_square_accurate():
     displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="accurate")
     assert int(displ[31,33,0]) == 0
     assert int(displ[31,33,1]) == 1
+
+def test_Correlation_3D():
+    """Do not move"""
+    im0 = np.zeros((64,64,64), np.float32)
+    im0[29:32, 30:33, 32:35] = 1
+    # A0, b0, c0 = make_Abc_fast(im0, 5)
+    # #c0
+    # assert np.unravel_index(np.argmax(c0), im0.shape) == (30,31,33)
+    # #b0
+    # np.testing.assert_almost_equal(b0[...,0].min(), b0[32,31,33,0])
+    # np.testing.assert_almost_equal(b0[...,0].max(), b0[29,31,33,0])
+    # np.testing.assert_almost_equal(b0[...,1].min(), b0[30,32,33,1])
+    # np.testing.assert_almost_equal(b0[...,1].max(), b0[30,29,33,1])
+    # np.testing.assert_almost_equal(b0[...,2].min(), b0[30,31,34,2])
+    # np.testing.assert_almost_equal(b0[...,2].max(), b0[30,31,31,2])
+    # #A0 diagonal
+    # np.testing.assert_almost_equal(A0[...,0,0].min(), A0[29,31,33,0,0])
+    # np.testing.assert_almost_equal(A0[...,0,0].max(), A0[32,31,33,0,0])
+    # np.testing.assert_almost_equal(A0[...,1,1].min(), A0[30,30,33,1,1])
+    # np.testing.assert_almost_equal(A0[...,1,1].max(), A0[30,29,33,1,1])
+    # np.testing.assert_almost_equal(A0[...,2,2].min(), A0[30,31,32,2,2])
+    # np.testing.assert_almost_equal(A0[...,2,2].max(), A0[30,31,31,2,2])
+    # #A0 must be symmetric
+    # for i in range(A0.shape[-2]):
+    #     for j in range(A0.shape[-1]):
+    #         assert np.all(A0[...,i,j]==A0[...,j,i])
+    #
+    #
+    # #identical images should not detect displacement
+    # A, Delta_b = prepare_displacement_matrices(A0, b0, A0, b0)
+    # assert np.all(A==A0)
+    # assert np.all(Delta_b==0)
+    displ, err = estimate_displacement(im0, im0, [5], [15], model="constant", method="accurate")
+    assert np.all(displ == 0)
+
+def test_Correlation_3D_shift_0():
+    """move a single square by 1 pixel along z"""
+    im0 = np.zeros((64,64,64), np.float32)
+    im0[29:32, 30:33, 32:35] = 1
+    #shift by one pixel on axis 0
+    im1 = np.zeros_like(im0)
+    im1[30:33, 30:33, 32:35] = 1
+    displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="accurate")
+    np.testing.assert_almost_equal(displ[30,31,33,0], 1,1)
+    np.testing.assert_almost_equal(displ[30,31,33,1], 0)
+    np.testing.assert_almost_equal(displ[30,31,33,2], 0)
+    #initial guess of the displacement
+    d0 = np.zeros_like(displ)
+    d0[...,0] = 1
+    d0 = displ
+    displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="accurate", d0=d0)
+    np.testing.assert_almost_equal(displ[30,31,33,0], 1,0)
+    np.testing.assert_almost_equal(displ[30,31,33,1], 0)
+    np.testing.assert_almost_equal(displ[30,31,33,2], 0)
+
+def test_Correlation_3D_shift_1():
+    """move a single square by 1 pixel along y"""
+    im0 = np.zeros((64,64,64), np.float32)
+    im0[29:32, 30:33, 32:35] = 1
+    #shift by one pixel on axis 0
+    im1 = np.zeros_like(im0)
+    im1[29:32, 31:34, 32:35] = 1
+    displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="accurate")
+    np.testing.assert_almost_equal(displ[30,31,33,0], 0)
+    np.testing.assert_almost_equal(displ[30,31,33,1], 1,1)
+    np.testing.assert_almost_equal(displ[30,31,33,2], 0)
+    #initial guess of the displacement
+    d0 = np.zeros_like(displ)
+    d0[...,1] = 1
+    d0 = displ
+    displ, err = estimate_displacement(im0, im1, [5], [15], model="constant", method="accurate", d0=d0)
+    np.testing.assert_almost_equal(displ[30,31,33,0], 0)
+    np.testing.assert_almost_equal(displ[30,31,33,1], 1,0)
+    np.testing.assert_almost_equal(displ[30,31,33,2], 0)
